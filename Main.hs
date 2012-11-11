fb4608ce62c6f10a500987a46574805a87c032d3
tranma <ma.ngoc.tran@gmail.com>
2012-11-11 17:26:21 +1100

    UI working, thanks liamoc

--- Main.hs
+++ Main.hs
@@ -1,48 +1,64 @@
-{-# LANGUAGE QuasiQuotes, ScopedTypeVariables #-}
+{-# LANGUAGE QuasiQuotes, ScopedTypeVariables, TupleSections, FlexibleContexts #-}
 
 -- Repa
 import Data.Array.Repa (Z (..), (:.) (..))
 import qualified Data.Array.Repa                 as R
-
+import qualified Data.Array.Repa.Repr.Vector     as R
 -- Gloss
 import Graphics.Gloss              
 import Graphics.Gloss.Raster.Array 
 import Graphics.Gloss.Interface.Pure.Game  
-
+-- JuicyPixels-repa
+import qualified Codec.Picture.Repa as J
 -- base
 import Control.Monad
 import System.Random
+import Data.Maybe
 
 -- friends
 import World
 import Step
 import Draw
 
+import Data.Word
+
 main :: IO ()                     
-main = playArrayIO
-  (InWindow "Falling Turnip" (resX * round factor, resY * round factor) (pos, pos))
-  (round factor, round factor)
-  frameRate
-  (World { array = R.computeS $ R.fromFunction (Z :. resY :. resX) bareWorld
-         , currentElem     = nothing
-         , currGravityMask = margMaskEven
-         , nextGravityMask = margMaskOdd
-         , mouseDown       = False
-         , mousePos        = (0,0) 
-         , mousePrevPos    = (0,0) }) 
-  ( return    . render)
-  ((return .) . handleInput)
-  stepWorld
+main = do  
+  tooltips <- mapM loadTooltip tooltipFiles
+  playArrayIO
+    (InWindow "Falling Turnip" (winX * round factor, winY * round factor) (pos, pos))
+    (round factor, round factor)
+    frameRate
+    (World { array = R.computeS $ R.fromFunction (Z :. resY :. resX) bareWorld
+           , currentElem     = nothing
+           , currGravityMask = margMaskEven
+           , nextGravityMask = margMaskOdd
+           , mouseDown       = False
+           , mousePos        = (0,0) 
+           , mousePrevPos    = (0,0) 
+           , tooltipLeft     = blankTooltip 
+           , tooltipRight    = blankTooltip 
+           }) 
+    ( return    . render)
+    ((return .) . handleInput)
+    (stepWorld tooltips)
   where frameRate = 30
         pos       = 300
         bareWorld = const nothing
 
+loadTooltip :: (Element, FilePath) -> IO (Element, R.Array R.V R.DIM2 Color)
+loadTooltip (e, p) = liftM ((e,) . either (error) fromJuicy) $ J.readImageRGBA p
+  where toF :: Word8 -> Float
+        toF x = fromIntegral x / 255
+        fromJuicy :: J.Collapsable a (Word8, Word8, Word8, Word8) => J.Img a -> R.Array R.V R.DIM2 Color
+        fromJuicy = (R.computeS . flip . R.map (\(a,b,c,d) -> makeColor (toF b) (toF c) (toF d) (toF a) ) . J.collapseColorChannel)
+        flip = R.backpermute (Z :. 15 :. 160) (\(Z:. y :. x) -> Z :. (14 - y) :. x )                 
 
 handleInput :: Event -> World -> World
 handleInput e w = handleInput' (w {mousePrevPos = mousePos w})
   where handleInput' world = case e of
-          EventKey (MouseButton LeftButton) Down _ (x,y) -> world { mouseDown = True, mousePos = (x/factor, y/factor) }
-          EventKey (MouseButton LeftButton) Up _   (x,y) -> world { mouseDown = False, mousePos = (x/factor, y/factor) }
+          EventKey (MouseButton LeftButton) Down _ (x,y) -> world { mouseDown = True, mousePos = (x/factor, y/factor - palletteH) }
+          EventKey (MouseButton LeftButton) Up _   (x,y) -> world { mouseDown = False, mousePos = (x/factor, y/factor - palletteH) }
           EventKey (Char 'e') Down _ _ -> world { currentElem = steam_water }
           EventKey (Char 'f') Down _ _ -> world { currentElem = fire        }
           EventKey (Char 'o') Down _ _ -> world { currentElem = oil         }
@@ -56,20 +72,32 @@ handleInput e w = handleInput' (w {mousePrevPos = mousePos w})
           EventKey (Char 'p') Down _ _ -> world { currentElem = plant       }
           EventKey (Char 'u') Down _ _ -> world { currentElem = spout       }
           EventKey (Char 'm') Down _ _ -> world { currentElem = metal       }
-          EventMotion (x,y) -> world { mousePos = (x/factor, y/factor) }
+          EventMotion (x,y) -> world { mousePos = (x/factor, y/factor - palletteH) }
           _ -> world
 
+blankTooltip :: R.Array R.V R.DIM2 Color
+blankTooltip = R.computeS $ R.fromFunction (Z :. 15 :. 160) (const black)
 
-stepWorld :: Float -> World -> IO World
-stepWorld time world
- = do int <- randomRIO (0,100)      
-      stepped <- if mouseDown world 
-                 then liftM   (step int $ currGravityMask world)
-                             $ drawLine (mousePrevPos world) (mousePos world) 
-                                        (currentElem  world) (array    world)
-                 else return $ step int (currGravityMask world) $ array world
-      array' <- R.computeP stepped
-      return $ world { array = array'
-                     , currGravityMask = nextGravityMask world
-                     , nextGravityMask = currGravityMask world } 
+handleUI :: [(Element, R.Array R.V R.DIM2 Color)] -> GlossCoord -> World -> World
+handleUI t p w = let tooltip = fromMaybe blankTooltip $ flip lookup t $ elemOf p
+                 in if mouseDown w then 
+                     w {currentElem = elemOf p, tooltipLeft = tooltip
+                                              , tooltipRight = tooltip }
+                    else w { tooltipRight = tooltip }
+
+stepWorld :: [(Element, R.Array R.V  R.DIM2 Color)] -> Float -> World -> IO World
+stepWorld tooltips time world
+ = let curr = mousePos world
+       world' = if outOfWorld curr then handleUI tooltips curr world else world { tooltipRight = blankTooltip }
+   in  do int     <- randomRIO (0,100)                            
+          stepped <- if   mouseDown world 
+                     then liftM (step int $ currGravityMask world')
+                             $  drawLine (mousePrevPos world') curr
+                                         (currentElem  world') (array world')
+                     else return $ step int (currGravityMask world') 
+                                 $ array world'
+          array'  <- R.computeP stepped
+          return $ world' { array = array'
+                          , currGravityMask = nextGravityMask world'
+                          , nextGravityMask = currGravityMask world' } 
 
--- World.hs
+++ World.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 module World 
        ( Element (..), Cell (..)
        , Env (..)
@@ -11,14 +12,19 @@ module World
 
        , MargPos (..)
        , GlossCoord (..), World (..)
-       , resX, resY, resWidth, resHeight, factor
-       , render ) 
+       , resX, resY, winX, winY, resWidth, resHeight, palletteH
+       , factor
+       , render, outOfWorld, elemOf, tooltipFiles ) 
 where
 
 import Graphics.Gloss    
 import Data.Word
-import Data.Array.Repa (D, U, DIM2, Array)
-import qualified Data.Array.Repa                 as R
+import Data.Array.Repa (Z (..), (:.) (..), D, U, DIM2, Array)
+import Data.Array.Repa.Repr.Vector
+import qualified Data.Array.Repa.Eval   as R
+import qualified Data.Array.Repa        as R
+
+import Data.List
 
 
 -- Basic constructs ------------------------------------------------------------
@@ -41,7 +47,9 @@ data World = World { array        :: Array U DIM2 Cell
                    , mousePos     :: GlossCoord 
                    , mousePrevPos :: GlossCoord 
                    , currGravityMask :: Array U DIM2 MargPos
-                   , nextGravityMask :: Array U DIM2 MargPos }
+                   , nextGravityMask :: Array U DIM2 MargPos 
+                   , tooltipLeft     :: Array V DIM2 Color 
+                   , tooltipRight    :: Array V DIM2 Color }
 
 
 -- Elements and properties -----------------------------------------------------
@@ -112,33 +120,96 @@ age gen x
 -- Drawing ---------------------------------------------------------------------
 
 render :: World -> Array D DIM2 Color
-render world = R.map color $ array world
-  where color :: Element -> Color
-        color 0   = black                                   -- nothing
-        color 1   = bright $ light $ light $ light blue     -- steam water           
-        color 2   = bright $ light $ light $ light blue     -- steam water           
-        color 6   = dark $ dim $ dim orange                 -- oil    
-        color 7   = bright $ bright $ light blue            -- water  
-        color 8   = bright $ bright $ light $ light blue    -- salt water
-        color 9   = dim yellow                              -- sand   
-        color 10  = greyN 0.95                              -- salt   
-        color 11  = greyN 0.7                               -- stone  
-        color 100 = greyN 0.4                               -- wall   
-        color 23  = bright $ orange                         -- torch
-        color 24  = dim $ green                             -- plant
-        color 25  = blue                                    -- spout
-        color 26  = mixColors (0.2) (0.8) blue (greyN 0.5)  -- metal
-        color x                                             -- fire
-          | isFire x  = mixColors (1.0 * fromIntegral (x - fire)) 
-                                  (1.0 * fromIntegral (fire_end - x)) 
-                                  red yellow 
-          | otherwise = error "render: element doesn't exist"
+render world 
+  = R.transpose $ (R.transpose $ tooltipLeft world R.++ R.map (dim . dim) (tooltipRight world)) 
+             R.++ (R.transpose buttons)
+             R.++ (R.transpose $ R.map colour $ array world) 
+             
 
-resX, resY :: Int
+
+colour :: Element -> Color
+colour 0   = black                                   -- nothing
+colour 1   = bright $ light $ light $ light blue     -- steam           
+colour 2   = bright $ light $ light $ light blue     -- steam condensed           
+colour 6   = dark $ dim $ dim orange                 -- oil    
+colour 7   = bright $ bright $ light blue            -- water  
+colour 8   = bright $ bright $ light $ light blue    -- salt water
+colour 9   = dim yellow                              -- sand   
+colour 10  = greyN 0.95                              -- salt   
+colour 11  = greyN 0.7                               -- stone  
+colour 100 = greyN 0.4                               -- wall   
+colour 23  = bright $ orange                         -- torch
+colour 24  = dim $ green                             -- plant
+colour 25  = blue                                    -- spout
+colour 26  = mixColors (0.2) (0.8) blue (greyN 0.5)  -- metal
+colour x                                             -- fire
+  | isFire x  = mixColors (1.0 * fromIntegral (x - fire)) 
+                          (1.0 * fromIntegral (fire_end - x)) 
+                          red yellow 
+  | otherwise = error "render: element doesn't exist"
+
+
+buttons :: Array V DIM2 Color
+buttons = R.fromList (Z :. buttonH + 4 :. resX) 
+        $  hPadding  ++ hPadding2
+       ++ (concat $ map oneLine [1..buttonH])
+       ++  hPadding2 ++ hPadding
+  where bgUI      = black
+        gap       = replicate 2 bgUI        
+        side      = replicate 5 bgUI
+        hPadding  = replicate resX white
+        hPadding2 = replicate resX bgUI
+        oneBox e  = oneBox' $ colour e
+        oneBox' c = replicate buttonW c
+        oneLine x = let col = mixColors (fromIntegral x / fromIntegral buttonH) 
+                                        (1.0 - fromIntegral x / fromIntegral buttonH)
+                                        red yellow
+                    in  side ++ (concat $ intersperse gap $ oneBox' col : map oneBox elems) ++ side
+        elems = [ wall, nothing, oil
+                , water, sand, salt, stone
+                , torch, plant, spout, metal ]
+
+resX, resY, buttonW, buttonH :: Int
 resX      = 320
 resY      = 240
+winX      = resX
+winY      = resY + buttonH + 4 + 15
 resWidth  = resX `div` 2
 resHeight = resY `div` 2
+buttonW = 24
+buttonH = 15
 
-factor :: Float
-factor = 1
+factor, palletteH :: Float
+factor = 2
+palletteH = (fromIntegral buttonH + 4 + 15)/2
+
+outOfWorld :: GlossCoord -> Bool
+outOfWorld (_, y) = round y + resHeight < 0
+
+elemOf :: GlossCoord -> Element
+elemOf ((subtract 5) . (+ resWidth) . round -> x, _)
+  | x < buttonW               = fire
+  | x <      2 + 2  * buttonW = wall
+  | x < 2  * 2 + 3  * buttonW = nothing
+  | x < 3  * 2 + 4  * buttonW = oil
+  | x < 4  * 2 + 5  * buttonW = water
+  | x < 5  * 2 + 6  * buttonW = sand
+  | x < 6  * 2 + 7  * buttonW = salt
+  | x < 7  * 2 + 8  * buttonW = stone
+  | x < 8  * 2 + 9  * buttonW = torch
+  | x < 9  * 2 + 10 * buttonW = plant
+  | x < 10 * 2 + 11 * buttonW = spout 
+  | otherwise                 = metal  
+
+tooltipFiles =[(fire    , "tooltips/fire.png"),
+               (wall    , "tooltips/wall.png"),
+               (nothing , "tooltips/erase.png"),
+               (oil     , "tooltips/oil.png"),
+               (water   , "tooltips/water.png"),
+               (sand    , "tooltips/sand.png"),
+               (salt    , "tooltips/salt.png"),
+               (stone   , "tooltips/stone.png"),
+               (torch   , "tooltips/torch.png"),
+               (plant   , "tooltips/plant.png"),
+               (spout   , "tooltips/spout.png"),
+               (metal   , "tooltips/metal.png")]
