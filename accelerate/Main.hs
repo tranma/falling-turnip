971768eeb3f047590cd84a59f64aec09575e151a
Tran Ma <tranma@cse.unsw.edu.au>
2013-06-26 15:03:59 +1000

    use run1

--- accelerate/Main.hs
+++ accelerate/Main.hs
@@ -5,7 +5,6 @@ import qualified Data.Array.Repa                 as R
 import qualified Data.Array.Repa.Repr.Vector     as R
 
 import Data.Array.Accelerate.IO
-import qualified Data.Array.Accelerate           as A
 import qualified Data.Array.Accelerate.CUDA      as C
 
 import Graphics.Gloss
@@ -48,15 +47,13 @@ stepWorld tooltips time world
        world' = if outOfWorld curr
                 then handleUI tooltips curr world
                 else world { tooltipRight = blankTooltip }
+       step' int acc = step int (currGravityMask world') acc
    in  do int     <- randomRIO (0,100)
-          stepped <- if   mouseDown world
-                     then do drawn <- drawLineA (mousePrevPos world') curr
-                                                (currentElem  world') (array world')
-                             return $ step int (currGravityMask world') (A.use $ fromRepa  drawn)
-                     else return $ step int (currGravityMask world')
-                                 $ A.use $ fromRepa $ array  world'
-          array'  <- return $ C.run stepped
-          return $ world' { array = toRepa $ array'
+          drawn   <- if   mouseDown world
+                     then drawLineA (mousePrevPos world') curr
+                                    (currentElem  world') (array world')
+                     else return $ array world'
+          return $ world' { array = toRepa $ C.run1 (step' int) (fromRepa drawn)
                           , currGravityMask = nextGravityMask world'
                           , nextGravityMask = currGravityMask world' }
 
